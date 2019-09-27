ee0355bed9fb19d3b347bcc7fc25eb948cc6f121
Kiran,Garag <kiran.garag@cerner.com>
2019-09-27 08:43:03 +0530

    First submission

---
+++ c language/BaseValueRaiseToPower.c
@@ -0,0 +1,20 @@
+// cerner_2^5_2019
+//Program for base_value ^ power_value
+
+#include <stdio.h>
+
+int main()
+{
+    int base_value,power_value;;
+    int i=1;
+    long result=1;
+
+    printf("Enter and power value respectively");
+    scanf("%d%d",&base_value,&power_value);
+    
+    for(i=1;i<=power_value;i++)
+     result = result*base_value;
+     
+    printf("result = %d",result);
+    return 0;
+}
