2ebb205beb8c06828d80eae837dc1741e29a56aa
Kiran,Garag <kiran.garag@cerner.com>
2019-10-14 18:37:18 +0530

    fourth submission

---
+++ Python/mathFuntions.py
@@ -0,0 +1,12 @@
+import math
+print(math.sqrt(6))
+print(math.ceil(5.9))
+print(math.floor(5.6))
+print (math.pow(2,5))
+print(math.log10(1))
+print(math.sin(90))
+print(math.degrees(90))
+print(math.sinh(90))
+print(math.exp(5))
+print(math.fabs(10.8))
+print(math.factorial(10))
